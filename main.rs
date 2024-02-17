a840283308b6a6ae17a2761910b7c898c169cf4d
Alex Donn <wndsrfr21@gmail.com>
2024-02-17 02:41:57 -0700

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,16 @@
+
+
+fn main() {
+    let sum = add(5, 3);
+    println!("5 + 3 = {}", sum);
+
+    match divide(10, 2) {
+        Ok(result) => println!("10 / 2 = {}", result),
+        Err(e) => println!("Error: {}", e),
+    }
+
+    match divide(10, 0) {
+        Ok(result) => println!("Result: {}", result),
+        Err(e) => println!("Error: {}", e),
+    }
+}
